20c393eb71f0102faed2bd7d2639cf7b2d150769
mazhekin <mazhekin@gmail.com>
2018-02-05 18:37:55 +0200

    load module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,19 +24,16 @@ export class AppComponent implements OnInit {
   }
 
   onLazy1Click() {
-    this.container.clear();
-    this.loader.load('app/lazy/lazy.module#LazyModule').then((moduleFactory: NgModuleFactory<any>) => {
-      const entryComponent = (<any>moduleFactory.moduleType).entry;
-      const moduleRef = moduleFactory.create(this.injector);
-
-      const compFactory = moduleRef.componentFactoryResolver.resolveComponentFactory(entryComponent);
-      this.container.createComponent(compFactory);
-    });
+    this.loadModule('app/lazy/lazy.module#LazyModule');
   }
 
   onLazy2Click() {
+    this.loadModule('app/lazy2/lazy2.module#Lazy2Module');
+  }
+
+  private loadModule(path: string) {
     this.container.clear();
-    this.loader.load('app/lazy2/lazy2.module#Lazy2Module').then((moduleFactory: NgModuleFactory<any>) => {
+    this.loader.load(path).then((moduleFactory: NgModuleFactory<any>) => {
       const entryComponent = (<any>moduleFactory.moduleType).entry;
       const moduleRef = moduleFactory.create(this.injector);
 
